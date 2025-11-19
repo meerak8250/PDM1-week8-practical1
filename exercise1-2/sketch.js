67626dd9d87bec23e37c6898776ea4af2111f596
Meera Kofkin <meerakofkin05@gmail.com>
2025-11-19 12:09:37 +0000

    completed 1-2

--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -2,7 +2,11 @@ let x = 50;
 let y = 50;
 let speedX = 3;
 let speedY = 3;
+let boing;
 
+function preload(){
+    boing = loadSound("assets/159376__greenhourglass__boing1.wav")
+}
 function setup() {
     createCanvas(600, 400);
 }
@@ -11,9 +15,11 @@ function draw() {
     background(255);
     circle(x, y, 100);
     if (x < 50 || x > width - 50) {
+        boing.play();
         speedX *= -1;
     }
     if (y < 50 || y > height - 50) {
+        boing.play();
         speedY *= -1;
     }
     x += speedX;
